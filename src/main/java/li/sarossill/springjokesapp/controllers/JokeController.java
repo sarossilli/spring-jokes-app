3d1f114a58560fb7056a60458928f5b764be71d7
Sam Rossilli <Sarossilli@gmail.com>
2022-01-08 14:23:02 -0700

    Implement jokes controller

---
+++ src/main/java/li/sarossill/springjokesapp/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package li.sarossill.springjokesapp.controllers;
+
+import li.sarossill.springjokesapp.services.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokesService jokesService;
+
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokesService.getJoke());
+        return "index";
+    }
+
+}
