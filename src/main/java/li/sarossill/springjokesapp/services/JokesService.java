c5493f4267dd751c518d0f0abaee87619914f72e
Sam Rossilli <Sarossilli@gmail.com>
2022-01-08 14:19:49 -0700

    Create Jokes Service

---
+++ src/main/java/li/sarossill/springjokesapp/services/JokesService.java
@@ -0,0 +1,5 @@
+package li.sarossill.springjokesapp.services;
+
+public interface JokesService {
+    String getJoke();
+}
---
+++ src/main/java/li/sarossill/springjokesapp/services/JokesServiceImpl.java
@@ -0,0 +1,19 @@
+package li.sarossill.springjokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+
+    private final ChuckNorrisQuotes quotes;
+
+    public JokesServiceImpl() {
+        this.quotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return quotes.getRandomQuote();
+    }
+}
